19d2340f73063f6feb27db6f61ae5ac99020ea4d
mathias-bridgman <mathias.bridgman9@gmail.com>
2023-09-11 15:18:48 +1200

    tried adding game needs work

---
+++ client/components/Game.tsx
@@ -0,0 +1,36 @@
+import { Key, ReactElement, JSXElementConstructor, ReactNode } from 'react'
+import { turnFormat } from '../../models/game'
+import { useState } from 'react';
+
+const [noughtsArr, setNoughtsArr] = useState<number[]>([]);
+const [crossesArr, setCrossesArr] = useState<number[]>([]);
+
+const Grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+<div className="grid-container">
+  {Grid.map((boxNumber: boolean | Key ) => (
+    <button
+      type="button"
+      key={boxNumber}
+      value={boxNumber}
+      onClick={handleClick}
+    >
+      {boxNumber}
+    </button>
+  ))}
+</div>
+
+const winArrays = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+function handleClick(event: MouseEvent<HTMLButtonElement, MouseEvent>): void {
+  throw new Error('Function not implemented.');
+}
---
+++ models/game.ts
@@ -0,0 +1,4 @@
+export interface turnFormat {
+  Player1: number
+  Player2: number
+}
